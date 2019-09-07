6e7e3cbb2fe066852a398bcc9a47216bd92ce77d
bambangoke <bambangeko05@gmail.com>
2019-09-07 15:53:19 +0700

    change data

--- public/js/data.js
+++ public/js/data.js
@@ -4,27 +4,26 @@ const data = {
             text: `Louis Iaeger V`,
             link: `#app`
         },
-        navLinks: [
+        navLinks: [{
+                name: `Home`,
+                url: `#app`
+            },
             {
-                name:   `Home`,
-                url:    `#app`
-            }, 
+                name: `About`,
+                url: `#about`
+            },
             {
-                name:   `About`,
-                url:    `#about`
-            }, 
+                name: `Projects`,
+                url: `#projects`
+            },
             {
-                name:   `Projects`,
-                url:    `#projects`
-            }, 
-            {
-                name:   `Contact`,
-                url:    `#contact`
+                name: `Contact`,
+                url: `#contact`
             }
         ]
     },
     jumbo: {
-        text: `Hi, I'm Louis. I design & build websites.`,
+        text: `Hi, I'm Bambang. I design & build websites.`,
         buttonText: `Yes, I'm available for hire.`,
         buttonLink: `#contact`,
         scrollDownLink: '#about',
@@ -36,112 +35,111 @@ const data = {
     },
     portfolio: {
         backgroundImg: `img/zipper.png`,
-        projects: [
-        {
-            title:          `Breakout`,
-            description:    `Breakout arcade game made w/PhaserJS Framework`,
-            img:            `https://github.com/louisiaegerv/Project-Images/blob/master/breakout.png?raw=true`,
-            link:           `https://louisiaegerv.github.io/breakout/`,
-            tags:           [
-                `JavaScript`, `Game Dev`
-            ]
-        },
-        {
-            title:          `LouTube`,
-            description:    `Simple YouTube Clone made with React`,
-            img:            `https://github.com/louisiaegerv/Project-Images/blob/master/loutube-wide.png?raw=true`,
-            link:           `https://www.youtube.com/watch?v=nr0FQeXYmws&feature=youtu.be`,
-            tags:           [
-                `JavaScript`, `React`
-            ]
-        },
-        {
-            title:          `guestlist`,
-            description:    `Manage your party's RSVP's with this responsive web app made with JavaScript!`,
-            img:            `https://github.com/louisiaegerv/Project-Images/raw/master/GuestListApp.jpg?raw=true`,
-            link:           `https://louisiaegerv.github.io/guestlist/`,
-            tags:           [
-                `JavaScript`, `localstorage`
-            ]
-        },
-        {
-            title:          `compass`,
-            description:    `A responsive demo website with Google Maps API, Bootstrap, and jQuery UI.`,
-            img:            `https://github.com/louisiaegerv/Project-Images/raw/master/compassDesktop.png`,
-            link:           `https://louisiaegerv.github.io/compass`,
-            tags:           [
-                `JavaScript`, `APIs`, `jQuery`
-            ]
-        },
-        {
-            title:          `stocks-nodejs`,
-            description:    `Get any stocks current NASDAQ price with this NodeJS app.`,
-            img:            `https://github.com/louisiaegerv/Project-Images/blob/master/stock2.png?raw=true`,
-            link:           `https://github.com/louisiaegerv/stocks-nodejs`,
-            tags:           [
-                `JavaScript`, `NodeJS`, `APIs`
-            ]
-        },
-        {
-            title:          `weather-nodejs`,
-            description:    `Get the temp and weather for any ZIP with this NodeJS application.`,
-            img:            `https://raw.githubusercontent.com/louisiaegerv/Project-Images/master/weather2.png`,
-            link:           `https://github.com/louisiaegerv/weather-nodejs`,
-            tags:           [
-                `JavaScript`, `nodeJS`, `APIs`
-            ]
-        },
-        {
-            title:          `quote`,
-            description:    `Random quote generator made with JavaScript & Twitter API.`,
-            img:            `https://github.com/louisiaegerv/Project-Images/raw/master/quoteApp.jpg`,
-            link:           `https://louisiaegerv.github.io/quote`,
-            tags:           [
-                `JavaScript`, `APIs`
-            ]
-        },
-        {
-            title:          `resume`,
-            description:    `My responsive online resume`,
-            img:            `https://github.com/louisiaegerv/resume/raw/master/assets/img/my-resume.png`,
-            link:           `https://louisiaegerv.github.io/resume`,
-            tags:           [
-                `SASS`
-            ]
-        },
-        {
-            title:          `Zephyr layout`,
-            description:    `The responsive and mobile-first template site I created with ReactJS, HTML5, and CSS.`,
-            img:            `https://github.com/louisiaegerv/Project-Images/raw/master/ZephyrSite.jpg`,
-            link:           `https://louisiaegerv.github.io/zephyr/`,
-            tags:           [
-                `JavaScript`, `ReactJS`
-            ]
-        },
-        {
-            title:          `Cobalt layout`,
-            description:    `Another responsive website theme I created using HTML and CSS`,
-            img:            `https://raw.githubusercontent.com/louisiaegerv/Project-Images/master/cobalt.png`,
-            link:           `https://louisiaegerv.github.io/cobalt`,
-            tags:           [
-                `JavaScript`
-            ]
-        },
-        {
-            title:          `noteboard`,
-            description:    `For all those genius ideas you keep forgetting.`,
-            img:            `https://raw.githubusercontent.com/louisiaegerv/Project-Images/master/noteboard.png`,
-            link:           `https://louisiaegerv.github.io/noteboard`,
-            tags:           [
-                `JavaScript`, `jQuery`
-            ]
-        }
-    ]},
+        projects: [{
+                title: `Breakout`,
+                description: `Breakout arcade game made w/PhaserJS Framework`,
+                img: `https://github.com/louisiaegerv/Project-Images/blob/master/breakout.png?raw=true`,
+                link: `https://louisiaegerv.github.io/breakout/`,
+                tags: [
+                    `JavaScript`, `Game Dev`
+                ]
+            },
+            {
+                title: `LouTube`,
+                description: `Simple YouTube Clone made with React`,
+                img: `https://github.com/louisiaegerv/Project-Images/blob/master/loutube-wide.png?raw=true`,
+                link: `https://www.youtube.com/watch?v=nr0FQeXYmws&feature=youtu.be`,
+                tags: [
+                    `JavaScript`, `React`
+                ]
+            },
+            {
+                title: `guestlist`,
+                description: `Manage your party's RSVP's with this responsive web app made with JavaScript!`,
+                img: `https://github.com/louisiaegerv/Project-Images/raw/master/GuestListApp.jpg?raw=true`,
+                link: `https://louisiaegerv.github.io/guestlist/`,
+                tags: [
+                    `JavaScript`, `localstorage`
+                ]
+            },
+            {
+                title: `compass`,
+                description: `A responsive demo website with Google Maps API, Bootstrap, and jQuery UI.`,
+                img: `https://github.com/louisiaegerv/Project-Images/raw/master/compassDesktop.png`,
+                link: `https://louisiaegerv.github.io/compass`,
+                tags: [
+                    `JavaScript`, `APIs`, `jQuery`
+                ]
+            },
+            {
+                title: `stocks-nodejs`,
+                description: `Get any stocks current NASDAQ price with this NodeJS app.`,
+                img: `https://github.com/louisiaegerv/Project-Images/blob/master/stock2.png?raw=true`,
+                link: `https://github.com/louisiaegerv/stocks-nodejs`,
+                tags: [
+                    `JavaScript`, `NodeJS`, `APIs`
+                ]
+            },
+            {
+                title: `weather-nodejs`,
+                description: `Get the temp and weather for any ZIP with this NodeJS application.`,
+                img: `https://raw.githubusercontent.com/louisiaegerv/Project-Images/master/weather2.png`,
+                link: `https://github.com/louisiaegerv/weather-nodejs`,
+                tags: [
+                    `JavaScript`, `nodeJS`, `APIs`
+                ]
+            },
+            {
+                title: `quote`,
+                description: `Random quote generator made with JavaScript & Twitter API.`,
+                img: `https://github.com/louisiaegerv/Project-Images/raw/master/quoteApp.jpg`,
+                link: `https://louisiaegerv.github.io/quote`,
+                tags: [
+                    `JavaScript`, `APIs`
+                ]
+            },
+            {
+                title: `resume`,
+                description: `My responsive online resume`,
+                img: `https://github.com/louisiaegerv/resume/raw/master/assets/img/my-resume.png`,
+                link: `https://louisiaegerv.github.io/resume`,
+                tags: [
+                    `SASS`
+                ]
+            },
+            {
+                title: `Zephyr layout`,
+                description: `The responsive and mobile-first template site I created with ReactJS, HTML5, and CSS.`,
+                img: `https://github.com/louisiaegerv/Project-Images/raw/master/ZephyrSite.jpg`,
+                link: `https://louisiaegerv.github.io/zephyr/`,
+                tags: [
+                    `JavaScript`, `ReactJS`
+                ]
+            },
+            {
+                title: `Cobalt layout`,
+                description: `Another responsive website theme I created using HTML and CSS`,
+                img: `https://raw.githubusercontent.com/louisiaegerv/Project-Images/master/cobalt.png`,
+                link: `https://louisiaegerv.github.io/cobalt`,
+                tags: [
+                    `JavaScript`
+                ]
+            },
+            {
+                title: `noteboard`,
+                description: `For all those genius ideas you keep forgetting.`,
+                img: `https://raw.githubusercontent.com/louisiaegerv/Project-Images/master/noteboard.png`,
+                link: `https://louisiaegerv.github.io/noteboard`,
+                tags: [
+                    `JavaScript`, `jQuery`
+                ]
+            }
+        ]
+    },
     contact: {
         email: `louis.iaeger@yahoo.com`
     },
-    social: [
-        {
+    social: [{
             name: 'twitter',
             shortlink: 'twitter.com/theCodeslayer/',
             fullLink: 'https://twitter.com/theCodeslayer/',
@@ -156,24 +154,24 @@ const data = {
             fontAwesome: true
         },
         {
-            name:       'linkedin',
-            shortlink:  'linkedin.com/in/louislinkedin',
-            fullLink:   'https://www.linkedin.com/in/louislinkedin',
-            img:        '',
+            name: 'linkedin',
+            shortlink: 'linkedin.com/in/louislinkedin',
+            fullLink: 'https://www.linkedin.com/in/louislinkedin',
+            img: '',
             fontAwesome: true
         },
         {
-            name:       'Codecademy',
-            shortlink:  'codecademy.com/megapandalord',
-            fullLink:   'https://www.codecademy.com/megapandalord',
-            img:        'https://louisiaegerv.github.io/resume/assets/img/cc2w.png',
+            name: 'Codecademy',
+            shortlink: 'codecademy.com/megapandalord',
+            fullLink: 'https://www.codecademy.com/megapandalord',
+            img: 'https://louisiaegerv.github.io/resume/assets/img/cc2w.png',
             fontAwesome: false
         },
         {
-            name:       'Team Treehouse',
-            shortlink:  'teamtreehouse.com/louisiaeger',
-            fullLink:   'https://teamtreehouse.com/louisiaeger',
-            img:        'https://raw.githubusercontent.com/louisiaegerv/Project-Images/master/treehouse.png',
+            name: 'Team Treehouse',
+            shortlink: 'teamtreehouse.com/louisiaeger',
+            fullLink: 'https://teamtreehouse.com/louisiaeger',
+            img: 'https://raw.githubusercontent.com/louisiaegerv/Project-Images/master/treehouse.png',
             fontAwesome: false
         }
     ]
